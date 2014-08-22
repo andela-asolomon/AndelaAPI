25a5e9ae0ade1101f635cc74a9c768ef89ddd18a
Seun Martins <seun@impactmass.com>
2014-08-22 08:46:12 +0100

    Admin roles functions

--- public/modules/admin/config/admin.client.routes.js
+++ public/modules/admin/config/admin.client.routes.js
@@ -5,9 +5,13 @@ angular.module('admin').config(['$stateProvider',
   function($stateProvider) {
     // Lists state routing
     $stateProvider.
-    state('createAdmin', {
+    state('createUser', {
       url: '/admin/create',
       templateUrl: 'modules/admin/views/admin.create.users.html'
+    }).
+    state('adminWelcome', {
+      url: '/admin/welcome',
+      templateUrl: 'modules/admin/views/admin.welcome.html'
     });
   }
 ]);
--- public/modules/admin/controllers/admin.client.controller.js
+++ public/modules/admin/controllers/admin.client.controller.js
@@ -17,6 +17,93 @@ angular.module('admin').controller('AdminController', ['$scope', '$http',
         console.log('Error - can not');
       });
     };
+
+    $scope.viewTrainees = function() {
+      console.log('viewTrainees called');
+      $http.get('/admin/trainees').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listApplicants = function() {
+      $http.get('/admin/applicants').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listApplicants = function() {
+      $http.get('/admin/fellows').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listFellows = function() {
+      $http.get('/admin/fellows').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listInstructors = function() {
+      $http.get('/admin/instructors').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listAdmins = function() {
+      $http.get('/admin/admins').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.viewApplicant = function(apptId) {
+      $http.get('/admin/appt/' + apptId).success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        $scope.passwordDetails = null;
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    
   
   }
 ]);
