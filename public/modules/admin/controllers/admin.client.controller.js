f86033b4d347a5182c2332f44fa8660cbb6229b6
Seun Martins <seun@impactmass.com>
2014-08-24 00:33:26 +0100

    Add functions to admin panel

--- public/modules/admin/controllers/admin.client.controller.js
+++ public/modules/admin/controllers/admin.client.controller.js
@@ -1,9 +1,12 @@
 'use strict';
 
 // Lists controller
-angular.module('admin').controller('AdminController', ['$scope', '$http',
-  function($scope, $http) {
-    
+angular.module('admin').controller('AdminController', ['$scope', '$http', 'Authentication',
+  function($scope, $http, Authentication) {
+
+    $scope.user = Authentication.user;
+
+
     // Create new user
     $scope.create = function() {
       console.log('createInstructor called');
@@ -17,6 +20,103 @@ angular.module('admin').controller('AdminController', ['$scope', '$http',
         console.log('Error - can not');
       });
     };
-  
+
+    $scope.viewTrainees = function() {
+      console.log('viewTrainees called');
+      $http.get('/admin/trainees').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listApplicants = function() {
+      $http.get('/admin/applicants').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listApplicants = function() {
+      $http.get('/admin/fellows').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listFellows = function() {
+      $http.get('/admin/fellows').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listInstructors = function() {
+      $http.get('/admin/instructors').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.listAdmins = function() {
+      $http.get('/admin/admins').success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    };
+
+    $scope.viewApplicant = function(apptId) {
+      $http.get('/admin/appt/' + apptId).success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        console.log('Success - Done', response);
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    }; 
+
+    $scope.changeRole = function() {
+      console.log($scope.apptId);
+      // console.log(typeof $scope.credentials.role);
+      $http.put('/admin/appt/' + $scope.apptId + '/role', $scope.appt.role).success(function(response) {
+        // If successful show success message and clear form
+        $scope.success = true;
+        
+      }).error(function(response) {
+        $scope.error = response.message;
+        console.log('Error - can not');
+      });
+    }; 
   }
 ]);
