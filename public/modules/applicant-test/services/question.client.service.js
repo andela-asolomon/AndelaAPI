d9b90fa4139eed5e9759e382bd3de59a5086e311
dtytomi <dtytomi@gmail.com>
2014-08-24 00:57:56 +0100

    Menu control bar working and applicant

---
+++ public/modules/applicant-test/services/question.client.service.js
@@ -0,0 +1,7 @@
+'use strict';
+
+//Questions service used for communication with the articles REST endpoints
+angular.module('applicant-test').factory('Articles', ['$resource',
+    function($resource) {
+        return $resource('')
+    }])
