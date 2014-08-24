836fe74af792edb3f76d518e1e6fc5531ebba769
Seun Martins <seun@impactmass.com>
2014-08-24 18:11:28 +0100

    Add views for admin actions on applicants

--- public/modules/admin/config/admin.client.routes.js
+++ public/modules/admin/config/admin.client.routes.js
@@ -16,6 +16,24 @@ angular.module('admin').config(['$stateProvider',
     state('adminWelcome', {
       url: '/admin/welcome',
       templateUrl: 'modules/admin/views/admin.welcome.html'
+    }).
+    state('viewAppts', {
+      url: '/admin/appts',
+      templateUrl: 'modules/admin/views/list.applicants.admin.html'
+    }).
+    state('viewAppt', {
+      url: '/admin/appt/:apptId',
+      templateUrl: 'modules/admin/views/view.applicant.admin.html'
+    }).
+    state('editAppt', {
+      url: '/admin/editappt/:apptId',
+      templateUrl: 'modules/admin/views/edit.appt.form.admin.html'
+    }).
+    state('viewTrainee', {
+      url: '/admin/trainees',
+      templateUrl: 'modules/admin/views/view.trainees.admin.html'
     });
+    
   }
 ]);
+
